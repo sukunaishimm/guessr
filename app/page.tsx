56ce442b06f85fb6363f10e045765999b8e1fd9f
SUKUNAISHIMM <hardeygreattj@yahoo.com>
2025-11-16 04:42:18 +0100

    Delete app directory

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div className="p-8 text-xl">
-      <p>✅ Your Frame is live!</p>
-      <p>Endpoint: <code>/api/frame</code></p>
-    </div>
-  );
-}
